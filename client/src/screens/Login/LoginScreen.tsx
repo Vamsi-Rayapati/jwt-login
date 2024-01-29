f0b322a5ddc724bb9f230d371d657c6801e0255a
Vamsi Rayapati <rkrishna@nouveau-labs.com>
2024-01-29 10:10:34 +0530

    code+

---
+++ client/src/screens/Login/LoginScreen.tsx
@@ -0,0 +1,49 @@
+import Lottie from 'react-lottie'
+
+import animationData from '../../assets/animations/communication_animation.json';
+import { Button, Card, Checkbox, Form, Input } from 'antd';
+
+function LoginScreen() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+  return (
+    <div style={{display:'flex', flexDirection:'row', alignItems:'center', height:'100%', paddingRight: 100}}>
+        <Lottie options={defaultOptions} width={600} height={500}/>
+        <Card title="Login" style={{}}>
+
+        <Form>
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+                Submit
+            </Button>
+            </Form.Item>
+        </Form>
+        </Card>
+    </div>
+  )
+}
+
+export default LoginScreen
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,53 @@
 // src/index.ts
 import express, { Request, Response } from 'express';
+import logger from './middlewares/logger';
 
 const app = express();
-const PORT = 3000;
-
+const PORT = 4000;
+app.use(express.json());
+app.use(logger);
 let name="Vamsi";
+let users = [
+  {
+    name:'Vamsi',
+    email:'vamsi@gmail.com'
+  },
+  {
+    name:'Sai',
+    email:'sai@gmail.com'
+  },
+  {
+    name:'Prasanth',
+    email:'prasanth@gmail.com'
+  }
+]
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express with TypeScript!');
 });
 
-app.get('/user', (req: Request, res: Response) => {
-  res.send(name)
+app.post('/user', (req: Request, res: Response) => {
+  console.log('VRRRRR', req.body);
+  const user = req.body;
+  users.push({
+    name: user.name,
+    email: user.email
+  });
+  res.send(201)
 });
+
+app.get('/users', (req: Request, res: Response) => {
+
+  res.send({
+    users: users,
+    total: users.length
+  })
+});
+
+app.get('/users/:index', (req: Request, res: Response) => {
+  const index: number = Number(req.params.index);
+  res.send(users[index])
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
