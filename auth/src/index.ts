10bda305da3e4d2b0d0a330975f5bd58cb062665
Vamsi Rayapati <rkrishna@nouveau-labs.com>
2024-02-15 09:59:11 +0530

    restructure

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,14 @@
 // src/index.ts
 import express, { Request, Response } from 'express';
 import logger from './middlewares/logger';
+import path from 'path';
 
 const app = express();
 const PORT = 4000;
 app.use(express.json());
 app.use(logger);
 let name="Vamsi";
-let users = [
+const users = [
   {
     name:'Vamsi',
     email:'vamsi@gmail.com'
@@ -25,14 +26,33 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express with TypeScript!');
 });
 
+app.get('/file',(req: Request,res:Response)=> {
+  res.sendFile(path.join(__dirname,'../public','sample.html'));
+});
+
 app.post('/user', (req: Request, res: Response) => {
-  console.log('VRRRRR', req.body);
-  const user = req.body;
-  users.push({
-    name: user.name,
-    email: user.email
-  });
-  res.send(201)
+ const user = req.body;
+  if(!user.email || !user.name) {
+    res.status(400).send("Please include email and name");
+  } else {
+    users.push({
+      name: user.name,
+      email: user.email
+    });
+    res.send(201)
+  }
+  
+});
+
+app.put('/user/:id',(req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  if(id<users.length) {
+    users[id]=req.body
+    res.send(200);
+  } else {
+    res.status(404).send('User Not found');
+  }
+
 });
 
 app.get('/users', (req: Request, res: Response) => {
@@ -44,8 +64,10 @@ app.get('/users', (req: Request, res: Response) => {
 });
 
 app.get('/users/:index', (req: Request, res: Response) => {
-  const index: number = Number(req.params.index);
-  res.send(users[index])
+  const index: number = parseInt(req.params.index);
+  const user = users[index];
+  if(user)res.send(user);
+  else res.status(404).send('User Not found');
 });
 
 app.listen(PORT, () => {
